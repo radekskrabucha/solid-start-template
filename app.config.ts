c273a4d028e6824301d3250e97a9dbe76f2b1d1c
radekskrabucha <rskrabucha13@gmail.com>
2024-05-07 13:07:29 +0200

    feat(tailwind): added tailwind v4

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,9 @@
 import { ValidateEnv } from '@julr/vite-plugin-validate-env'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from '@solidjs/start/config'
 
 export default defineConfig({
   vite: {
-    plugins: [ValidateEnv({})]
+    plugins: [ValidateEnv({}), tailwindcss()]
   }
 })
