d0aa8ff88cd18337de6d4e7934f6f4f44deee0c8
radekskrabucha <rskrabucha13@gmail.com>
2024-05-14 13:14:30 +0200

    feat: addex axios and tanstack/solid-query

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,16 @@
 import { Router } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start/router'
+import { QueryClientProvider } from '@tanstack/solid-query'
 import { MainLayout } from './layout/MainLayout'
+import { queryClient } from './lib/solidQuery'
 import './styles/app.css'
 
 export default function App() {
   return (
-    <Router root={props => <MainLayout>{props.children}</MainLayout>}>
-      <FileRoutes />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router root={props => <MainLayout>{props.children}</MainLayout>}>
+        <FileRoutes />
+      </Router>
+    </QueryClientProvider>
   )
 }
