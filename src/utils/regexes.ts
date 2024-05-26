a755043ad39f4996c565de7155cb150503fe1ecf
radekskrabucha <rskrabucha13@gmail.com>
2024-05-26 14:54:13 +0200

    fix(regexes): fixed isExternalLink

--- src/utils/regexes.ts
+++ src/utils/regexes.ts
@@ -1,2 +1,3 @@
 export const isExternalLink = (url: string) =>
-  /^(http|https|ftp|mailto|tel):/.test(url)
+  /^(http|https|ftp|mailto|tel):/.test(url) ||
+  /^[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/.test(url)
